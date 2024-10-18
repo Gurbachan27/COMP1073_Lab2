5132ab4d057671397f7a3a3836b6a153173c2a92
Gurbachan Singh Parmar <gurbachanparmarbinjon@gmail.com>
2024-10-18 11:12:03 -0400

    Initial commit

---
+++ js/page-color-changer.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Constants for the input sliders and body element
+    const redSlider = document.getElementById('red');
+    const greenSlider = document.getElementById('green');
+    const blueSlider = document.getElementById('blue');
+    const body = document.body;
+
+   
+});
